aa9a1ad1684238a49fb6dac0fc435a0a841152df
Erik Roming <eroming3@gmail.com>
2022-03-30 20:14:20 -0500

    Established the recipe-search-app with initial file structure and packages

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { RecipeHomePage } from './features'
+
+function App() {
+  return (
+    <div>Hello World</div>
+  );
+}
+
+export default App;
